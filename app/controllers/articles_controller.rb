49939105bdbbb6683432bf2566d1d1dfa54aae8c
Mauricio P <mauriciopdvg@gmail.com>
2019-06-04 21:10:08 -0300

    Update all views with categories

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,7 +4,7 @@ class ArticlesController < ApplicationController
 	before_action :require_same_user, only: [:edit, :update, :destroy]
 
 	def index
-		@articles = Article.paginate(page: params[:page], per_page: 5)	
+		@articles = Article.paginate(page: params[:page], per_page: 5)
 	end
 
 	def new
@@ -26,24 +26,24 @@ class ArticlesController < ApplicationController
 	end
 
 	def update
-		
+
 		if @article.update(article_params)
 			flash[:success] = "Article was successfully updated"
 			redirect_to article_path(@article)
 		else
 			render 'edit'
-		end	
+		end
 	end
 
 	def show
-		
+
 	end
 
 	def destroy
-		
+
 		@article.destroy
 		flash[:danger] = "Article was successfully deleted"
-		redirect_to articles_path		
+		redirect_to articles_path
 	end
 
 	private
@@ -51,7 +51,7 @@ class ArticlesController < ApplicationController
 			@article = Article.find(params[:id])
 		end
 		def article_params
-			params.require(:article).permit(:title, :description)
+			params.require(:article).permit(:title, :description, category_ids: [])
 		end
 		def require_same_user
 			if current_user != @article.user and !current_user.admin?
