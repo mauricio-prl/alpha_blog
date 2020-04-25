04042c9a96216519e91e155f7595aaae565496d9
Mauricio Pereira <mauriciopdvg@gmail.com>
2020-04-25 01:30:52 -0300

    Add seed file example (#15)

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+admin = User.where(username: 'admin').first_or_create(
+  email: 'admin@email.com',
+  password: '123456'
+)
+
+user1 = User.where(username: 'user_1').first_or_create(
+  email: 'user1@email.com',
+  password: '123456'
+)
+
+user2 = User.where(username: 'user_2').first_or_create(
+  email: 'user2@email.com',
+  password: '123456'
+)
+
+category1 = Category.where(name: 'Programming').first_or_create
+category2 = Category.where(name: 'Fishing').first_or_create
+
+article1 = user1.articles.where(title: 'Hello').first_or_create(
+  description: 'Hello guys! This is an example!',
+  category_ids: [category1.id, category2.id]
+)
+
+article2 = user2.articles.where(title: 'Hi!').first_or_create(
+  description: 'This is another example!',
+  category_ids: [category1.id, category2.id]
+)
